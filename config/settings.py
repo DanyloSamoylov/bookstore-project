9d2cf3909831d2ae619944f31b2fce5064b9183a
DanyloSamoylov <danylosamoylov@gmail.com>
2022-05-19 11:55:49 +0300

    ch12

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,6 @@ DEBUG = env.bool("DJANGO_DEBUG")
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -136,6 +135,9 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
